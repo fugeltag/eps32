1da03cbc932383facbf3dcf25d16f38769c1c412
fugeltag <arieleliyhou5@gmail.com>
2024-12-24 19:33:02 +0200

    Create sender.py
    the code of the esp that sends the data

---
+++ sender.py
@@ -0,0 +1,22 @@
+import network
+from machine import Pin
+import espnow
+import utime
+
+
+sta = network.WLAN(network.STA_IF)  
+sta.active(True)
+
+esp = espnow.ESPNow()
+esp.active(True)
+
+peer = b'\xe4e\xb8\x83V\xf4'
+esp.add_peer(peer)
+while(1):
+    
+    message = "hi"
+    print("Sending command :",message)
+    esp.send(peer, message)
+    utime.sleep(5)
+
+        
